3ded1e3a1a27bf144be963fd1a9a3fa8642593b6
mahimagontalwar <mahimagontalwar1209@gmail.com>
2024-07-27 16:02:21 +0530

    upadated to 2024

---
+++ ecom/src/context/MovieContext.js
@@ -0,0 +1,11 @@
+
+import {createContext} from "react";
+
+const MovieContext = createContext({
+  watchlist: [],
+  setWatchList: () => {},
+  handleAddToWatchList: () => {},
+  deleteFromWatchList: () => {},
+});
+
+export default MovieContext;
---
+++ ecom/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import Banner from "../../components/Banner";
+import Movies from "../../components/Movies";
+function HomePage() {
+  return(<>
+    <Banner />
+    <Movies />
+  </>)
+}
+
+export default HomePage;
